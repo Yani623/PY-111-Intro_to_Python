ca219d8b113b0cb35bac637c99533715efa52995
yani623 <yanirahagstrom@gmail.com>
2021-05-18 14:52:59 -0500

    Second Commit of the day. Adding Activity 7

---
+++ Activity7-YaniHagstrom.py
@@ -0,0 +1,42 @@
+#Create a function named add that expects two arguments
+#Return the sum of those two arguments
+
+def add(a = 11, b = 17):
+    sum = a + b
+    return sum
+
+print(add())
+
+
+#Create a function named subtract that expects two arguments
+#Return the difference of those two arguments
+
+def subtract(a = 11, b = 17):
+    difference = a - b
+    return difference
+
+print(subtract())
+
+
+#Create a function named multipy that expects two arguments
+#Return the product of those two arguments
+
+def multiply(a = 11, b = 17):
+    product = a * b
+    return product
+
+print(multiply())
+
+
+#Create a function named divide that expects two arguments
+#Return the product of those two arguments
+
+def divide(a = 11, b = 17):
+    quotient = a/b
+    return quotient
+
+print(divide())
+
+
+
+
---
+++ Day6_pyFunctions.py
@@ -0,0 +1,38 @@
+#Defining a function
+    #def FUNCTION NAME ():
+        ## Consider if this function needs a parameter/argument
+
+# def printStuff():
+#     print("Hello World")
+#     print("Yani")
+
+# printStuff()
+    # output =  Hello World
+    #           Yani
+
+########################
+
+## assigning a value. name is the placeholder and "Yani" is the parameter.
+def printIt(name = "Yani"):
+    print(f"Hi my name is {name}")
+    ## f in parenthesis is the f string function. 
+
+printIt()
+printIt("Camila")
+    ## output =  Hi my name is Yani
+    ##           Hi my name is Camila
+
+
+
+########################
+
+# Return 
+
+def gimmeFive():
+    return 5
+
+print(type(gimmeFive()))
+    ##output = <class 'int'>
+
+
+

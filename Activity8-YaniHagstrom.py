2328e2d445ee12da8e935946d264107c7ef95075
yani623 <yanirahagstrom@gmail.com>
2021-05-19 14:08:39 -0500

    Day 7 Commit

---
+++ Activity8-YaniHagstrom.py
@@ -0,0 +1,67 @@
+#Write a Program to check the greatest among 3 numbers
+
+# from _typeshed import ReadableBuffer
+
+
+# a = 5
+
+# b = 15
+
+# c = 25
+
+# if(a > b and a > c):
+#     print("A is the greatest")
+
+# elif(b > a and b > c):
+#     print("B is the greatest")
+
+# else:
+#     print ("C is the greatest")
+
+
+
+#Write a program to imitate a Traffic Light. 
+#Attempt 1:
+
+# r = 5
+
+# y = 15
+
+# g = 25
+
+# if(r > y and r > g):
+#     print("Red means Stop")
+
+# elif(y > r and y > g):
+#     print("Yellow means Slow Down")
+
+# else:
+#     print ("Green means Go")
+
+    ##Output is "Green means go"
+
+# the light is red if it is not green or yellow
+#Attempt 2
+
+control = 0
+
+red = 1
+
+green = 2
+
+if(control == red):
+    print("Red means Stop")
+
+elif(control == green):
+    print("Green means Go")
+
+else:
+    print("Yellow means Slow Down")
+
+    ##Output is "Yellow means Slow Down"
+
+
+
+
+
+
---
+++ Day7_Boolean.py
@@ -0,0 +1,54 @@
+#Practice with comparison operators.
+
+# sample1 = 4 > 9
+# sample2 = 2 < 4
+
+# print(sample1)
+# print(sample2)
+
+# Result in a boolean value.
+
+#Practice with logical operators
+
+# solution =  False and False
+# print(solution)
+#     #output is False
+#     ## only True if X & Y are both true.
+#     ##"False and False is the most False"
+
+# solution =  False or True
+# print(solution)
+#     #output is false
+
+
+#Practice Control Flow or If Statements:
+
+# IF
+#1: Write "if"
+#2: Follow with () with parameter/argument in ()
+#* do not need parenthesis around the argument if using python3 or greater
+#3: Put a colon at the end
+# always indent next line
+
+# if(True):
+#     name = "Yani"
+#     print("Hello World")
+#     print("Yani")
+#     print(name.lower())
+#     #Output == Hello World, Yani, yani
+
+#If shorthand
+    # if a > b: print("A is greater")
+    #* do not need parenthesis around the argument if using python3 or greater
+
+
+
+
+
+
+
+
+
+
+
+
